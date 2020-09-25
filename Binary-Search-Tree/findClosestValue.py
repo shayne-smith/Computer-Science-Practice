eb9ef0520f06643747f29d388bd2481b1c5e9e67
seanmx96 <seanmx96@users.noreply.github.com>
2020-09-25 14:46:58 -0500

    Implement solution to finding closest value to target value in BST

---
+++ Binary-Search-Tree/findClosestValue.py
@@ -0,0 +1,46 @@
+# Average: O(log(n)) time | O(log(n)) space
+# Worst: O(n) time | O(n) space
+
+# recursive solution
+def findClosestValueInBst(tree, target):
+    return findClosestValueInBstHelper(tree, target, float("inf"))
+
+def findClosestValueInBstHelper(tree, target, closest):
+    # base case
+    if tree is None:
+        return closest
+    if abs(target - closest) > abs(target - tree.value):
+        closest = tree.value
+    if target < tree.value:
+        return findClosestValueInBstHelper(tree.left, target, closest)
+    elif target > tree.value:
+        return findClosestValueInBstHelper(tree.right, target, closest)
+    else:
+        return closest
+
+# Average: O(log(n)) time | O(1) space
+# Worst: O(n) time | O(1) space
+
+# iterative solution
+def findClosestValueInBst(tree, target):
+    return findClosestValueInBstHelper(tree, target, float("inf"))
+
+def findClosestValueInBstHelper(tree, target, closest):
+    currentNode = tree
+    while current is not None:
+        if abs(target - closest) > abs(target - currentNode.value):
+            closest = currentNode.value
+        if target < currentNode.value:
+            currentNode = currentNode.left
+        elif target > currentNode.value:
+            currentNode = currentNode.right
+        else:
+            break
+    return closest
+
+# This is the class of the input tree. Do not edit.
+class BST:
+    def __init__(self, value):
+        self.value = value
+        self.left = None
+        self.right = None
