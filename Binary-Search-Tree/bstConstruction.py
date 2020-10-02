b4bce6241d085fb1ad16a28b9bf68c50b41ad9c7
seanmx96 <seanmx96@users.noreply.github.com>
2020-10-02 16:00:37 -0500

    create function to validate binary search tree

--- Binary-Search-Tree/bstConstruction.py
+++ Binary-Search-Tree/bstConstruction.py
@@ -5,7 +5,7 @@ class BST:
         self.right = None
 
     # Average: O(log(n)) time | O(log(n)) space
-    # Worst: O(n) time | O(n) space
+    # Worst: O(n) time | O(log(n)) space
     def insert(self, value):
         if value < self.value:
             if self.left is None:
@@ -19,8 +19,6 @@ class BST:
                 self.right.insert(value)
         return self
 
-    # Average: O(log(n)) time | O(log(n)) space
-    # Worst: O(n) time | O(n) space
     def contains(self, value):
         if value < self.value:
             if self.left is None:
@@ -35,42 +33,14 @@ class BST:
         else:
             return True
 
-    # Average: O(log(n)) time | O(log(n)) space
-    # Worst: O(n) time | O(n) space
-    def remove(self, value, parentNode=None):
+    def remove(self, value, parent=None):
         if value < self.value:
             if self.left is not None:
-                self.left.remove(value, self)
+                self.left.remove(self.value, self)
         elif value > self.value:
             if self.right is not None:
-                self.right.remove(value, self)
-        else:
-            # 2 child nodes present
-            if self.left is not None and self.right is not None:
-                # self.value = smallest value of right subtree
-				self.value = self.right.getMinValue()
                 self.right.remove(self.value, self)
-            # we're gonna come back to the root node case
-            elif parentNode is None:
-                if self.left is not None:
-                    self.value = self.left.value
-                    self.right = self.left.right
-                    self.left = self.left.left
-                elif self.right is not None:
-                    self.value = self.right.value
-                    self.left = self.right.left
-                    self.right = self.right.right
-                # remove root node without any children
-                else:
-                    pass
-            elif parentNode.left == self:
-                parentNode.left = self.left if self.left is not None else self.right
-            elif parentNode.right == self:
-                parentNode.right = self.left if self.left is not None else self.right
-        return self
-
-    def getMinValue(self):
-        if self.left is None:
-            return self.value
         else:
-            return self.left.getMinValue()
+            # has 2 children
+            if self.left is not None and self.right is not None:
+                self.value = self.right.getMinValue()
