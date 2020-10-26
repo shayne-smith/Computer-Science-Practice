0d77d4d1029de57f7dc470b16737596825bef34d
seanmx96 <seanmx96@users.noreply.github.com>
2020-10-26 16:47:36 -0500

    Implement naive solution for constructing minimal height BST.

---
+++ Binary-Search-Tree/minHeightBst.py
@@ -0,0 +1,36 @@
+# Time O(nlog(n)) | Space O(n)
+# Recursive, naive solution that uses BST insert method
+def minHeightBst(array):
+	return constructMinHeightBst(array, None, 0, len(array) - 1)
+
+def constructMinHeightBst(array, bst, startIdx, endIdx):
+	# base case
+	if endIdx < startIdx:
+		return
+	midIdx = (startIdx + endIdx) // 2
+	valueToAdd = array[midIdx]
+	if bst is None:
+		bst = BST(valueToAdd)
+	else:
+		bst.insert(valueToAdd)
+	constructMinHeightBst(array, bst, startIdx, midIdx - 1)
+	constructMinHeightBst(array, bst, midIdx + 1, endIdx)
+	return bst
+
+class BST:
+    def __init__(self, value):
+        self.value = value
+        self.left = None
+        self.right = None
+
+    def insert(self, value):
+        if value < self.value:
+            if self.left is None:
+                self.left = BST(value)
+            else:
+                self.left.insert(value)
+        else:
+            if self.right is None:
+                self.right = BST(value)
+            else:
+                self.right.insert(value)
