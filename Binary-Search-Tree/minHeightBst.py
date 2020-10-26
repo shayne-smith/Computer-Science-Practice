3fb2b33a79b187d4d5a405447fe834896f7b32d7
seanmx96 <seanmx96@users.noreply.github.com>
2020-10-26 18:10:20 -0500

    Implement efficient solution to minimum height bst problem.

--- Binary-Search-Tree/minHeightBst.py
+++ Binary-Search-Tree/minHeightBst.py
@@ -1,5 +1,5 @@
-# Time O(nlog(n)) | Space O(n)
-# Recursive, naive solution that uses BST insert method
+# Time O(n) | Space O(n)
+# Recursive, efficient solution that manually inserts new nodes
 def minHeightBst(array):
 	return constructMinHeightBst(array, None, 0, len(array) - 1)
 
@@ -10,23 +10,78 @@ def constructMinHeightBst(array, bst, startIdx, endIdx):
 
     # calculate middle index and value
 	midIdx = (startIdx + endIdx) // 2
-	valueToAdd = array[midIdx]
 
-    # check if bst is None
-	if bst is None:
-
-        # establish root node if so
-		bst = BST(valueToAdd)
-	else:
-
-        # otherwise add value to existing bst 
-		bst.insert(valueToAdd)
+    newBstNode = BST(array[midIdx])
+    if bst is None:
+        bst = newBstNode
+    else:
+        if array[midIdx] < bst.value:
+            bst.left = newBstNode
+            bst = bst.left
+        else:
+            bst.right = newBstNode
+            bst = bst.right
 
     # recursively call constructMinHeightBst with updated start and stop indices
 	constructMinHeightBst(array, bst, startIdx, midIdx - 1)
 	constructMinHeightBst(array, bst, midIdx + 1, endIdx)
     
 	return bst
+
+# def constructMinHeightBst(array, bst, startIdx, endIdx):
+# 	# base case
+# 	if endIdx < startIdx:
+# 		return
+
+#     # calculate middle index and value
+# 	midIdx = (startIdx + endIdx) // 2
+
+#     newBstNode = BST(array[midIdx])
+#     if bst is None:
+#         bst = newBstNode
+#     else:
+#         if array[midIdx] < bst.value:
+#             bst.left = newBstNode
+#             bst = bst.left
+#         else:
+#             bst.right = newBstNode
+#             bst = bst.right
+
+#     # recursively call constructMinHeightBst with updated start and stop indices
+# 	constructMinHeightBst(array, bst, startIdx, midIdx - 1)
+# 	constructMinHeightBst(array, bst, midIdx + 1, endIdx)
+    
+# 	return bst
+
+# # Time O(nlog(n)) | Space O(n)
+# # Recursive, naive solution that uses BST insert method
+# def minHeightBst(array):
+# 	return constructMinHeightBst(array, None, 0, len(array) - 1)
+
+# def constructMinHeightBst(array, bst, startIdx, endIdx):
+# 	# base case
+# 	if endIdx < startIdx:
+# 		return
+
+#     # calculate middle index and value
+# 	midIdx = (startIdx + endIdx) // 2
+# 	valueToAdd = array[midIdx]
+
+#     # check if bst is None
+# 	if bst is None:
+
+#         # establish root node if so
+# 		bst = BST(valueToAdd)
+# 	else:
+
+#         # otherwise add value to existing bst 
+# 		bst.insert(valueToAdd)
+
+#     # recursively call constructMinHeightBst with updated start and stop indices
+# 	constructMinHeightBst(array, bst, startIdx, midIdx - 1)
+# 	constructMinHeightBst(array, bst, midIdx + 1, endIdx)
+
+# 	return bst
 
 class BST:
     def __init__(self, value):
