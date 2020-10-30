43aba4a39e0ba52068ec12b681e3d242922e9226
seanmx96 <seanmx96@users.noreply.github.com>
2020-10-30 13:31:14 -0500

    Implemented recursive depth-first search algorithm for graphs.

---
+++ AlgoExpert/Graphs/depthFirstSearchGraphs.py
@@ -0,0 +1,15 @@
+# Time O(v + e) | Space O(v)
+class Node:
+    def __init__(self, name):
+        self.children = []
+        self.name = name
+
+    def addChild(self, name):
+        self.children.append(Node(name))
+        return self
+
+    def depthFirstSearch(self, array):
+        array.append(self.name)
+		for child in self.children:
+			child.depthFirstSearch(array)
+		return array
