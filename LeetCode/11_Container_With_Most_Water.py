71aa7b0bbadabf9ff14d78390c6627ef2ecd3fe8
Shayne Smith <shayne.m.smith@vanderbilt.edu>
2021-02-16 20:28:56 -0600

    linear solution to container with most water array problem

---
+++ LeetCode/11_Container_With_Most_Water.py
@@ -0,0 +1,20 @@
+class Solution:
+    
+    # O(n) time | O(1) space
+    def maxArea(self, height: List[int]) -> int:
+        l, r = 0, len(height) - 1
+        res = 0
+        
+        while l < r:
+            
+            area = (r - l)*min(height[l], height[r])
+            
+            res = max(area, res)
+            
+            if height[l] < height[r]:
+                l += 1
+            else:
+                r -= 1
+            
+        return res
+        
