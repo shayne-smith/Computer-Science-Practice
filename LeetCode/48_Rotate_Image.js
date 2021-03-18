2c82a2811a1ea8d62dd0b7ff9184c628db4cddee
Shayne Smith <shayne.m.smith@vanderbilt.edu>
2021-03-18 08:24:02 -0500

    Finished solution JS solution to Rotate Matrix problem

--- LeetCode/48_Rotate_Image.js
+++ LeetCode/48_Rotate_Image.js
@@ -17,13 +17,10 @@ var rotate = function(matrix) {
         for (let i=0; i < n; i++) {
             for (let j=0; j < Math.floor(n/2); j++) {
                 let tmp = matrix[i][j];
-                
+                matrix[i][j] = matrix[i][n - 1 - j];
+                matrix[i][n - 1 - j] = tmp;
             };
         };
-        // nested for loops
-            // i values stay constant
-            // j values transform to (n - 1 - j)
-                // matrix[i][j] = matrix
     };
     
     transpose(matrix);
